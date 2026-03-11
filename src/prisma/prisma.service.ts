b3b83147481127d48cc66bedb94e422cff90670e
Spekzin <gabrielallanlopws@gmail.com>
2026-03-11 18:13:21 -0300

    prisma

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,23 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
+    const isServerless = Boolean(process.env.VERCEL || process.env.AWS_LAMBDA_FUNCTION_NAME);
+    if (isServerless) {
+      return;
+    }
+
     await this.$connect();
   }
 
   async onModuleDestroy() {
+    const isServerless = Boolean(process.env.VERCEL || process.env.AWS_LAMBDA_FUNCTION_NAME);
+    if (isServerless) {
+      return;
+    }
+
     await this.$disconnect();
   }
 }
